5240a7a3d07b18af35012723beb148789ea3927c
fonzi <fonzi@5-6--9.Bildung.zz>
2016-05-19 14:27:40 +0200

    10. Sitzung: Methoden getNext, getPrev, ArrayList Methoden anwenden 	add, remove, set, get

---
+++ src/my/eingabe/Sitzung.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.eingabe;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fonzi
+ */
+public class Sitzung {
+
+    private ArrayList<LernKarte> lKs = LernKarte.getAll();
+    private int aktuellerLKIndex = 0;
+
+    /**
+     * Methoden des ArrayList definiert
+     *
+     * @return
+     */
+    public ArrayList<LernKarte> getlKs() {
+        return lKs;
+    }
+
+    public LernKarte getAktuelleLernKarte() {
+        return lKs.get(aktuellerLKIndex);
+    }
+
+    public void setAktuelleLernKarte(LernKarte lK) {
+        lKs.set(aktuellerLKIndex, lK);
+    }
+
+    public void addLernKarte(LernKarte lK) {
+        lKs.add(lK);
+    }
+
+    public void removeLernKarte(LernKarte lK) {
+        lKs.remove(lK);
+    }
+
+    // Schritt nach vorne
+    public LernKarte getNextLernKarte() {
+        if (aktuellerLKIndex == lKs.size() - 1) {
+            aktuellerLKIndex = 0;
+        } else {
+            aktuellerLKIndex++;
+        }
+        return getAktuelleLernKarte();
+    }
+
+    // Schrit nach hinten
+    public LernKarte getPrevLernKarte() {
+        if (aktuellerLKIndex == 0) {
+            aktuellerLKIndex = lKs.size() - 1;
+        } else {
+            aktuellerLKIndex--;
+        }
+        return getAktuelleLernKarte();
+    }
+}
